b69dc9b50052d4159e8a7278073d121321159f0e
sudheesh vasudev <sudheeshyft@gmail.com>
2023-07-08 11:51:29 +0530

    this is before deletion delete_not_included

--- siteapp/urls.py
+++ siteapp/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('movie/<int:movie_id>',views.movie_detail,name='movie_detail'),
+    path("", views.index, name="index"),
+    path("movie/<int:movie_id>", views.movie_detail, name="movie_detail"),
+    path("signup/", views.signup, name="signup"),
+    path("login/", views.user_login, name="login"),
+    path("logout/", views.user_logout, name="logout"),
+    path("add_movie", views.add_movie, name="add_movie"),
+    path("movies/<int:movie_id>/delete/", views.delete_movie, name="delete_movie"),
 ]
--- siteapp/views.py
+++ siteapp/views.py
@@ -1,18 +1,82 @@
-from django.shortcuts import render
-from . models import Movie
-from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from .models import Movie
+from django.views.decorators.csrf import csrf_protect
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+
+from django.shortcuts import render, redirect
+from .forms import MovieForm
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Movie
+# views.py
+
+
+def delete_movie(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    
+    if request.method == 'POST':
+        movie.delete()
+        return redirect('movie_list')
+    
+    context = {'movie': movie}
+    return render(request, 'delete_movie.html', context)
+
+
+def add_movie(request):
+    if request.method == "POST":
+        form = MovieForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+    else:
+        form = MovieForm()
+
+    return render(request, "add_movie.html", {"form": form})
 
 
 def index(request):
+    # form = SignUpForm()
     movie = Movie.objects.all()
-    context = {
-        'movie_list': movie
-
-    }
-    return render(request, 'index.html', context)
+    context = {"movie_list": movie}
+    return render(request, "index.html", context)
 
 
 def movie_detail(request, movie_id):
     movie = Movie.objects.get(id=movie_id)
-    return render(request, 'movie_detail.html', {'movie': movie})
+    return render(request, "movie_detail.html", {"movie": movie})
+
+
+# views.py
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form": form})
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("index")
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {"form": form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("login")
